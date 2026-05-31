c4b76cc443e4ad011d76f978d056132aadb128b4
AbhishekGusain20 <abhishek.gusain2006@gmail.com>
2026-05-31 10:50:00 +0530

    Add files via upload

---
+++ NETFLIX.js
@@ -0,0 +1,7 @@
+document.getElementById("signin").addEventListener("click", function() {
+    alert("Netflix Dummy Sign In Page");
+});
+
+document.querySelector(".watch-btn").addEventListener("click", function() {
+    alert("Enjoy Watching!");
+});
